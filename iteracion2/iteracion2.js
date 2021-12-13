38b7c9a1eed7fc252d037ad52ad3922f02e248d4
Isabel Gutiérrez <gm.isabel@gmail.com>
2021-12-13 21:17:01 +0100

    iteracion2 hasta 2.2

---
+++ iteracion2/iteracion2.js
@@ -0,0 +1,21 @@
+//2.1 Inserta dinamicamente en un html un div vacio con javascript.
+
+let newDiv= document.createElement('div');
+document.body.appendChild(newDiv);
+
+//2.2 Inserta dinamicamente en un html un div que contenga una p con javascript.
+
+let newDiv1= document.createElement('div');
+let newContent = document.createElement('p');
+
+newDiv1.appendChild(newContent);
+document.body.appendChild(newDiv1);
+
+//2.3 Inserta dinamicamente en un html un div que contenga 6 p utilizando un loop con javascript.
+
+//2.4 Inserta dinamicamente con javascript en un html una p con el texto 'Soy dinámico!'.
+
+
+
+
+
